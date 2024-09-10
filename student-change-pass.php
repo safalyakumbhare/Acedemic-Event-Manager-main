5cd4017af0ebde0472132fd293df19adf8aac05a
Safalya Kumbhare <safalyakumbhare@gmail.com>
2024-09-10 20:24:59 +0530

    bug fixed

--- student-change-pass.php
+++ student-change-pass.php
@@ -11,6 +11,7 @@ include("header-student.php");
     <title>Change Password</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/animate.css">
 </head>
 <style>
     textarea:focus,
@@ -52,7 +53,7 @@ include("header-student.php");
 <body class="bg-black">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-6 h-50 p-5 bg-dark mt-5 rounded-3">
+            <div class="col-6 h-50 p-5 bg-dark wow fadeInUp mt-5 rounded-3">
                 <form action="changepass-student.php" method="POST">
                     
                     <div class="mb-3">
